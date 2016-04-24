5f47ede1c7a1355d0ebfc5ece323a8897aa234bc
Anna Whitney <awhitney@college.harvard.edu>
2016-04-24 11:22:48 -0400

    Repeated random written

--- randheuristics.ml
+++ randheuristics.ml
@@ -1,14 +1,25 @@
 open Solution
 
-let rec rep_random 
+let rep_random (lst : int list) : int =
+    let l = List.length lst in
+    let rec next_random iter best_res =
+        if iter = 0 then best_res else
+        let s = Soln.generate l in
+        let new_res = Soln.get_residue s in
+        if new_res < best_res then next_random (iter - 1) new_res
+        else next_random (iter - 1) best_res
+    in
+    next_random 25000 (Soln.get_residue (Soln.generate l))
+;;
 
 let main () =
     let soln_flag = Sys.argv.(1) in
-    let s = match soln_flag with
+    let soln = match soln_flag with
             | "S" -> (module StandardSoln : SOLUTION)
             | "P" -> (module PrepartitionSoln : SOLUTION)
     in
-    let module Soln = (val s : SOLUTION)
+    let module Soln = (val soln : SOLUTION)
     in
 
+;;
 
